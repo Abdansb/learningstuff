bca7275cad710801ad710d0c525620c640a9baeb
Abdansb <abdanxcx@gmail.com>
2023-03-21 04:28:07 +0700

    add new normalization script

---
+++ Math for ML/normal_distribution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Random Generation of 1000 independent Normal samples
+import numpy as np
+mu = 0
+sigma = 1
+N = 1000
+X = np.random.normal(mu,sigma,N)
+ 
+# Population distribution
+from scipy.stats import norm
+x_values = np.arange(-5,5,0.01)
+y_values = norm.pdf(x_values)
+#Sample histogram with Population distribution
+import matplotlib.pyplot as plt
+counts, bins, ignored = plt.hist(X, 30, density = True,color = 'purple',label = 'Sampling Distribution')
+plt.plot(x_values,y_values, color = 'y',linewidth = 2.5,label = 'Population Distribution')
+plt.title("Randomly generating 1000 obs from Normal distribution mu = 0 sigma = 1")
+plt.ylabel("Probability")
+plt.legend()
+plt.show()
