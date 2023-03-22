951fbefba1932defdfaf3780aeb7d3f7ad1242a7
Abdansb <abdanxcx@gmail.com>
2023-03-23 05:40:32 +0700

    Format document using black

--- Math for ML/normal_distribution.py
+++ Math for ML/normal_distribution.py
@@ -11,21 +11,23 @@ st = time.time()
 mu = 0
 sigma = 1
 N = 1000
-X = np.random.normal(mu,sigma,N)
- 
+X = np.random.normal(mu, sigma, N)
+
 # Population distribution
-x_values = np.arange(-5,5,0.01)
+x_values = np.arange(-5, 5, 0.01)
 y_values = norm.pdf(x_values)
 
-#Sample histogram with Population distribution
-counts, bins, ignored = plt.hist(X, 30, density = True,color = 'purple',label = 'Sampling Distribution')
-plt.plot(x_values,y_values, color = 'y',linewidth = 2.5,label = 'Population Distribution')
+# Sample histogram with Population distribution
+counts, bins, ignored = plt.hist(
+    X, 30, density=True, color="purple", label="Sampling Distribution"
+)
+plt.plot(x_values, y_values, color="y", linewidth=2.5, label="Population Distribution")
 plt.title("Randomly generating 1000 obs from Normal distribution mu = 0 sigma = 1")
 plt.ylabel("Probability")
 plt.legend()
 
 end = time.time()
 elapsed = end - st
-print('waktu run: ', elapsed, 'detik')
-#plt.show()
+print("waktu run: ", elapsed, "detik")
+# plt.show()
 futures.ThreadPoolExecutor().submit(plt.show())
